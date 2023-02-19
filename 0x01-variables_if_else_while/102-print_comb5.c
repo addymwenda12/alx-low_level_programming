a162cf9eec49b6acd347a8cdf3612bd4c1b57aa9
addymwenda12 <muneneaddy@gmail.com>
2023-02-19 05:19:18 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	{
+		for (i = 0 ; i <= 99 ; i++)
+		{
+			for (j = i ; j <= 99 ; j++)
+			{
+				if (i == j)
+				{
+					continue;
+				}
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((j / 10) + '0');
+				putchar((j % 10) + '0');
+				{
+					if (i < 99 || j < 99)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
