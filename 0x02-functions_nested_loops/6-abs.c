84a9258f34f80bc72abf1d8c5cec9e7da17915e1
addymwenda12 <muneneaddy@gmail.com>
2023-02-21 10:56:03 -0800

    There are only 3 colors

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 #include"main.h"
-
+#include<stdlib.h>
 /**
- * _abs - computes the absolute value of an integer
+ *_abs - computes the absolute value of an integer
  *
  * Return: the absolute value of int
  *
  */
 int _abs(int n)
 {
+	int n;
+
 	if (n >= 0)
 	{
 		return (n);
