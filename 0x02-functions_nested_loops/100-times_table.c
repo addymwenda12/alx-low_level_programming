29a3f41ce03ea0e6d8e529b526e81e99d5ebf4a9
addymwenda12 <muneneaddy@gmail.com>
2023-02-22 05:18:24 -0800

    The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,6 +8,16 @@
  */
 void print_times_table(int n)
 {
-
-	return (0);
+	if (n < 0 || n > 15)
+	{
+		return;
+	}
+	for (int i = 0 ; i <= n ; i++)
+	{
+		for (int j = 0 ; j <= n ; j++)
+		{
+std::count << i * j << "\t";
+		}
+std::cout << std::end1;
+	}
 }
