ee35ea1d20f7c481c0b8400751a1fac62005daa2
addymwenda12 <muneneaddy@gmail.com>
2023-02-22 06:10:15 -0800

    The World looks like a multiplication-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,16 +8,42 @@
  */
 void print_times_table(int n)
 {
-	if (n < 0 || n > 15)
+	int i, j, res;
+
+	if (!(n > 15 || n < 0))
 	{
-		return;
-	}
-	for (int i = 0 ; i <= n ; i++)
-	{
-		for (int j = 0 ; j <= n ; j++)
+		for (i = 0; i <= n; i++)
 		{
-std::count << i * j << "\t";
+			for (j = 0; j <= n; j++)
+			{
+				res = (i * j);
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (res < 10 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 10 && res < 100)
+				{
+					_putchar(' ');
+					_putchar((res / 10) + '0');
+					_putchar((res % 10) + '0');
+				}
+				else if (res >= 100 && j != 0)
+				{
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
+					_putchar((res % 10) + '0');
+				}
+				else
+					_putchar((res % 10) + '0');
+			}
+			_putchar('\n');
 		}
-std::cout << std::end1;
 	}
 }
