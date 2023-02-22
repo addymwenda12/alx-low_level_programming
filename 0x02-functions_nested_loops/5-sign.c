1587c1211f12436db194a705e451e5f661ddfba1
addymwenda12 <muneneaddy@gmail.com>
2023-02-22 04:44:31 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -17,19 +17,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
-		putchar('1');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar('0');
 		putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
 		putchar('-');
-		putchar('1');
-		putchar('-1');
+		return ('-1');
 	}
 }
