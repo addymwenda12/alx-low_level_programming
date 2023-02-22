ab916182b74a855765631234f7b594553231a843
addymwenda12 <muneneaddy@gmail.com>
2023-02-22 06:00:36 -0800

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -16,17 +16,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		putchar(43);
+		putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		putchar(48);
+		putchar('0');
 		return (0);
 	}
 	else if (n < 0)
 	{
-		putchar(45);
+		putchar('-');
 		return (-1);
 	}
 }
