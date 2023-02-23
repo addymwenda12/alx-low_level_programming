61cfada73ebec4f8f591af3cbdd4e29dbbf2f75e
addymwenda12 <muneneaddy@gmail.com>
2023-02-23 02:33:51 -0800

    I feel like I am diagonally parked in a parallel universe

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,18 +3,17 @@
 /**
  * print_diagonal - prints a diagonal line of backlashes in the terminal
  * @n: the number of times the backslash character should be printed
- *
+ * Return: a diagonal
  */
 
 void print_diagonal(int n)
 {
+	int i, j;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-
-	int i, j;
-
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
