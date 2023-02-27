0f042d4ac8a4c9155846720c5990beaa8c25de86
addymwenda12 <muneneaddy@gmail.com>
2023-02-27 04:08:20 -0800

    I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -20,7 +20,7 @@ void print_rev(char *s)
 		len++;
 
 	/* Print the string on reverse */
-	for (int i = len - 1; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 		_putchar(s[i]);
 
 	/* Prints a new line character to start a new line */
