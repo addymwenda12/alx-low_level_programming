a0fbe0f4875607fe1093367d183de2b49136a740
addymwenda12 <muneneaddy@gmail.com>
2023-02-27 04:01:54 -0800

    I can only go one way

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include"main.h"
+#include<string.h>
+
+/**
+ * print_rev - Prints a string in reverse, followed by a newline
+ *
+ * @s: The string to be printed in reverse
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	/* Get the length of the string */
+	while (s[len] != '\0')
+		len++;
+
+	/* Print the string on reverse */
+	for (int i = len - 1; i >= 0; i--)
+		_putchar(s[i]);
+
+	/* Prints a new line character to start a new line */
+	_putchar('\n');
+}
