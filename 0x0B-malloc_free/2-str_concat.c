102fcc2b127df8bef98da173625c421b27bc3424
addymwenda12 <muneneaddy@gmail.com>
2023-03-13 04:14:08 -0700

    Betty code checked

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,23 +3,23 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings
- * @s1: First string
- * @s2: Second string
- * Return: NULL on failure
- */
+* str_concat - concatenates two strings
+* @s1: First string
+* @s2: Second string
+* Return: NULL on failure
+*/
 
 char *str_concat(char *s1, char *s2)
 {
 	unsigned int len1 = strlen(s1);
 	unsigned int len2 = strlen(s2);
 	unsigned int len = len1 + len2 + 1;
-	char *ptr = malloc(len * sizeof(char));
+	char *ptr = malloc(sizeof(char) * (len1 + len2 + 1));
 
-	if (s1 == NULL)
-                s1 = "";
-        if (s2 == NULL)
-                s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 	if (!ptr)
 	{
 		return (NULL);
