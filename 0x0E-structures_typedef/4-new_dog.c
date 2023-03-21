429a7ea1b189868d8db6cb54fc4b23594a12761c
addymwenda12 <muneneaddy@gmail.com>
2023-03-21 04:11:46 -0700

    Error corrected

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -19,13 +19,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 	new_dog->name = malloc(sizeof(name) + 1);
-	if (new_dog == NULL)
+	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
 	new_dog->owner = malloc(sizeof(owner) + 1);
-	if (new_dog == NULL)
+	if (new_dog->owner == NULL)
 	{
 		free(new_dog->name);
 		free(new_dog);
