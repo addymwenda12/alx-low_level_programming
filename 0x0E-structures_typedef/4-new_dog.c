c2b1f8ae01bae3058ed39d6d1cf472bfa7b6398a
addymwenda12 <muneneaddy@gmail.com>
2023-03-21 04:04:52 -0700

    Adding function new_dog in the file 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+
+/**
+ * new_dog - create a new dog
+ * @name: new dog's name
+ * @age: new dog's age
+ * @owner: new dog's owner
+ * Return: pointer to new dog struct
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i;
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = malloc(sizeof(name) + 1);
+	if (new_dog == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = malloc(sizeof(owner) + 1);
+	if (new_dog == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i]; i++)
+		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
+	new_dog->age = age;
+	for (i = 0; owner[i]; i++)
+		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
+	return (new_dog);
+}
