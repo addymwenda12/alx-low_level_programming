f057951787b692b284cc5f87b12522dd5f91547b
addymwenda12 <muneneaddy.com>
2023-05-08 07:41:35 -0700

    Function that appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,6 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+
 		if (bytes_written == -1 || bytes_written != (ssize_t)strlen(text_content))
 		{
 			close(fd);
