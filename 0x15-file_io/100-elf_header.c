2af85e20af8e862aff4325ddf46b9c4c97b74fad
addymwenda12 <muneneaddy@gmail.com>
2023-07-10 14:56:06 -0700

    Program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -25,7 +25,7 @@ void display_error(const char *message)
 
 void display_elf_header(const char *filename)
 {
-	int fd;
+	int fd, i;
 	ssize_t bytes_read;
 
 	Elf64_Ehdr header;
@@ -43,19 +43,43 @@ void display_elf_header(const char *filename)
 
 	if (memcmp(header.e_ident, ELFMAG, SELFMAG) != 0)
 		display_error("Not an ELF file");
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
+	for (i = 0; i < EI_NIDENT; i++)
+		printf("%02x ", header.e_ident[i]);
+	printf("\n");
+	printf("Class:                             %s\n",
+			(header.e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("Data:                              %s\n",
+			(header.e_ident[EI_DATA] == ELFDATA2LSB) ?
+			"2's complement, little endian" : "2's complement, big endian");
+	printf("Version:                           %d (current)\n",
+			header.e_ident[EI_VERSION]);
+	printf("OS/ABI:                            %s\n", (header.e_ident[EI_OSABI]
+				== ELFOSABI_SYSV) ? "UNIX - System V" : "Other");
+	printf("ABI Ver:                       %d\n", header.e_ident[EI_ABIVERSION]);
+	printf("Type:                              ");
+	switch (header.e_type)
+	{
+		case ET_NONE:
+			printf("NONE (None)\n");
+			break;
+		case ET_REL:
+			printf("REL (Relocatable file)\n");
+			break;
+		case ET_EXEC:
+			printf("EXEC (Executable file)\n");
+			break;
+		case ET_CORE:
+			printf("CORE (Core file)\n");
+			break;
+		default:
+			printf("0x%04x\n", header.e_type);
+			break;
+	}
 
-	printf("Magic:   %c%c%c\n", header.e_ident[EI_MAG1],
-			header.e_ident[EI_MAG2], header.e_ident[EI_MAG3]);
-	printf("Class:   %d-bit\n", (header.e_ident[EI_CLASS]
-				== ELFCLASS32) ? 32 : 64);
-	printf("Data:    %s\n", (header.e_ident[EI_DATA] == ELFDATA2LSB)
-			? "2's complement, little endian" : "2's complement, big endian");
-	printf("Version: %d\n", header.e_ident[EI_VERSION]);
-	printf("OS/ABI:  %s\n", (header.e_ident[EI_OSABI] == ELFOSABI_SYSV)
-			? "UNIX - System V" : "Other");
-	printf("ABI Ver: %d\n", header.e_ident[EI_ABIVERSION]);
-	printf("Type:    0x%04x\n", header.e_type);
-	printf("Entry:   0x%lx\n", header.e_entry);
+	printf("  Entry point address:               0x%lx\n",
+			(unsigned long)header.e_entry);
 }
 
 /**
