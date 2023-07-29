6da7d256f38baa4d2e8f9e4e6928c40637bfb825
addymwenda12 <muneneaddy@gmail.com>
2023-07-28 23:30:00 -0700

    program that displays the information contained in the ELF header at the start of an ELF file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,8 +10,14 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void print_error(const char *message);
-void exit_with_error(int code, const char *message);
+void verify(unsigned char *e_ident);
+void magic(unsigned char *e_ident);
+void class(unsigned char *e_ident);
+void data(unsigned char *e_ident);
+void version(unsigned char *e_ident);
+void osabi(unsigned char *e_ident);
+void type(unsigned int e_type, unsigned char *e_ident);
+void entry(unsigned int e_entry, unsigned char *e_ident);
 int main(int argc, char *argv[]);
 
 #endif
